057996af472776234340892b0102d95cf453a1e5
Ravi Singh <ravigahlla@gmail.com>
2024-04-26 19:37:14 -0700

    added load_api_key()

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+import json
+
+def load_api_key(key):
+    with open('../.config', 'r') as file:
+        config = json.load(file)
+        return config[key]
 
 if __name__ == '__main__':
-    print("it works")
+    print(load_api_key('stratechery_email'))
