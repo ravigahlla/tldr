43f13673f4c43d494f974055ba582717640890dd
Ravi Singh <ravigahlla@gmail.com>
2024-05-25 14:17:06 -0700

    Updated setup.py to include dependencies recommended by GPT

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='stratecheryGPT',
     version='0.1',
     packages=[''],
     install_requires=[
-        'urllib3<2.0'  # a macOS issue, see https://github.com/urllib3/urllib3/issues/3020
+        'requests>=2.24.0',
+        'tiktoken>=0.1.0',  # Adjust the version as necessary
+        'openai>=0.10.2'  # Adjust the version as necessary
     ],
     url='',
     license='MIT',
     author='Ravinder Singh',
     author_email='ravigahlla@gmail.com',
-    description='a summarizer for Stratechery emails, using GPT'
+    description='a summarizer for Stratechery emails, using GPT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
